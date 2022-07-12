ce579d1810268344406be3c59faff9da75913147
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2022-07-11 23:06:26 -0400

    Update

--- script.js
+++ script.js
@@ -4,37 +4,69 @@ Created by Arnold Bradshaw
 11th July, 2022
 */
 
-// Constants go here
+/* 
+Constants go here
+*/
+
 const btnReset = document.querySelector('.btn-reset');
 const imgRock = document.querySelector('.rock')
 const imgPaper = document.querySelector('.paper')
 const imgScissors = document.querySelector('.scissors')
 const myScore = document.querySelector('.my-score')
 const pcScore = document.querySelector('.pc-score')
+const rockPaperScissorsDiv = document.querySelectorAll('.pic')
 let computerChoises = ['rock', 'paper', 'scissors']
 
-// Event listeners go here
+let score = {
+    Me: 0,
+    PC: 0,
+}
+
+/* 
+Event Listeners go here
+*/
+
 btnReset.addEventListener('click', (e) => {
-    console.log(e.target);
     myScore.innerText = 'My Score: ' + 0
     pcScore.innerText = 'PC Score: ' + 0
 })
 
 imgRock.addEventListener('click', (e) => {
-    console.log(e.target);
+    compareChoises()
 })
 imgPaper.addEventListener('click', (e) => {
-    console.log(e.target);
+    compareChoises()
 })
 imgScissors.addEventListener('click', (e) => {
-    console.log(e.target);
+    compareChoises()
 })
 
-// Functions go here
-// Randon generator for the PC
+/* 
+Functions go here
+*/
+// Computer generated choice via function
+randomGeneratedPCChoice = () => {
+    let computerChoice = computerChoises[Math.floor(Math.random() * 3).valueOf()];
+    console.log(computerChoice);
+}
 
-let computerChoice = () => Math.floor(Math.random() * 3) + 1;
-let compChoice = computerChoises[computerChoice()]
-console.log(compChoice);
-console.log(computerChoises[0]);
-console.log(computerChoice());
+// function to compare results
+compareChoises = () => {
+    let p = randomGeneratedPCChoice()
+    // I chose the clicked item so analyse the PC choice, p
+    switch (p) {
+        case p === 'rock':
+            console.log('Draw');
+            break;
+        case p === 'paper':
+            console.log("Win");
+            break;
+        default:
+            console.log("Lost");
+            break;
+    }
+}
+// Function to update the screen text
+
+
+// function to update scores
