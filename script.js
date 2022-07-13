053fd8db71ed6aab06ddc9d9cffcfeecc092d4b1
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2022-07-13 06:19:30 -0400

    Update

--- script.js
+++ script.js
@@ -13,32 +13,8 @@ const btnPaper = document.querySelector('.paper');
 const btnScissors = document.querySelector('.scissors');
 const myScore = document.querySelector('.my-score');
 const pcScore = document.querySelector('.pc-score');
-const rockPaperScissorsDiv = document.querySelectorAll('.pic');
+const alertText = document.querySelector('.alert-text');
 let choises = ['rock', 'paper', 'scissors'];
-
-// Define score
-let score = 0;
-
-/* 
-Event Listeners go here
-*/
-btnReset.addEventListener('click', (e) => {
-    resetGame();
-});
-
-btnRock.addEventListener('click', (e) => {
-    console.log(e.target.value);
-    getComputerChoice();
-});
-btnPaper.addEventListener('click', (e) => {
-    console.log(e.target.value);
-    getComputerChoice();
-
-});
-btnScissors.addEventListener('click', (e) => {
-    console.log(e.target.value);
-    getComputerChoice();
-});
 
 /* 
 Functions go here
@@ -49,27 +25,67 @@ getComputerChoice = () => {
     return computerChoice;
 };
 
+// Reset button to reset the game
+btnReset.addEventListener('click', (e) => {
+    endGame();
+});
+
+// function to compare results or get results
+getResult = (playerChoice, computerChoice) => {
+    let score = 0;
+    if (playerChoice == computerChoice) {
+        score = 0;
+    }
+    else if ((playerChoice == 0 && computerChoice == 1) || playerChoice == 0 && computerChoice == 2) {
+        score = 1;
+    }
+    else {
+        score = -1;
+    }
+    return score;
+};
+
+// Function to update the screen text
+showResult = (score, playerChoice, computerChoice) => {
+    if (playerChoice == computerChoice) {
+        score = 0;
+        alertText.innerText = "You DRAW";
+    } else if (playerChoice > computerChoice) {
+        score = 1;
+        alertText.innerText = "You WIN";
+    } else {
+        alertText.innerText = "You LOSE";
+        score = -1;
+    }
+};
+
+// player click RPS button
+playerClickRPS = () => {
+    btnRock.addEventListener('click', (e) => {
+        console.log(e.target.value);
+        getComputerChoice();
+    });
+    btnPaper.addEventListener('click', (e) => {
+        console.log(e.target.value);
+        getComputerChoice();
+
+    });
+    btnScissors.addEventListener('click', (e) => {
+        console.log(e.target.value);
+        getComputerChoice();
+    });
+};
+
+// funtion to play game
+playGame = () => {
+
+};
 
 // function to reset all the text to zero
-resetGame = () => {
+endGame = () => {
     myScore.innerText = 'My Score: ' + 0;
     pcScore.innerText = 'PC Score: ' + 0;
 };
 
-// function to compare results or get results
-
-getResult = (playerChoice, computerChoice) => {
-
-};
-
-
-// Function to update the screen text
-showResult = (score, playerChoice, computerChoice) => {
-
-};
-
-// player click RPS button
-playerClickRPS = () => { };
-
-// funtion to play game
-playGame = () => { };
+// Call the play game function
+playGame();
