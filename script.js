fc93cce390d92928618776e1de05870077afc8ff
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2022-07-14 15:53:51 -0400

    Update

--- script.js
+++ script.js
@@ -16,87 +16,87 @@ let alertText = document.querySelector('.alert-text');
 /* 
 Functions go here
 */
-// GLobal Scorekeeping
-let totalScore = { computerScore: 0, playerScore: 0 };
 
-// Computer generated choice via function
+// GLobal Scorekeeping
+let computerScore = 0;
+let playerScore = 0;
+
+// Computer generated choice via function, this returns the words Rock, Paper or Scissors
 getComputerChoice = () => {
     let rpsChoices = ['Rock', 'Paper', 'Scissors'];
     let computerChoice = rpsChoices[Math.floor(Math.random() * rpsChoices.length)];
     return computerChoice;
 };
 
-// function to compare results or get results
-getResult = (totalScore, playerChoice, computerChoice) => {
-    if (playerChoice === computerChoice) {
-        playerScore = 0;
-        computerScore = 0;
-    }
-    else if (playerChoice == 'Rock' && computerChoice == 'Scissors') {
-        playerScore = 1;
-        computerScore = 0;
-    } else if (playerChoice == 'Paper' && computerChoice == 'Rock') {
-        playerScore = 1;
-        computerScore = 0;
-    }
-    else if (playerChoice === 'Scissors' && computerChoice === 'Paper') {
-        playerScore = 1;
-        computerScore = 0;
-    }
-    else {
-        playerScore = -1;
-        computerScore = 1;
-    }
-    return totalScore;
+// Calculate who won
+playerClickRPS = (playerChoice) => {
+    let computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    console.log(playerChoice);
+    // let totalScore = getResult(playerChoice, computerChoice);
+    // showResult(totalScore, playerChoice, computerChoice);
+};
+
+playerClickRPS();
+
+// function to compare results and get results
+getResult = (playerChoice, computerChoice) => {
+    // console.log(computerChoice);
+    // console.log(playerChoice);
+    // if (playerChoice == computerChoice) {
+    //     playerScore = 0;
+    //     computerScore = 0;
+    // }
+    // else if (playerChoice == 'Rock' && computerChoice == 'Scissors') {
+    //     playerScore = playerScore + 1;
+    //     computerScore = 0;
+    // } else if (playerChoice == 'Paper' && computerChoice == 'Rock') {
+    //     playerScore = playerScore + 1;
+    //     computerScore = 0;
+    // }
+    // else if (playerChoice == 'Scissors' && computerChoice == 'Paper') {
+    //     playerScore = playerScore + 1;
+    //     computerScore = 0;
+    // }
+    // else {
+    //     playerScore = playerScore - 1;
+    //     computerScore = computerScore + 1;
+    // }
+    // console.log({ totalScore });
 };
 
 // Function to update the screen text
 showResult = (totalScore, playerChoice, computerChoice) => {
-
-    if (playerScore = 0) {
-        alertText.innerText = "DRAW";
-    } else if (playerScore = 1) {
-        alertText.innerText = "You WIN";
-    } else {
-        alertText.innerText = "You LOOSE";
-    }
-
-    myScore.innerText = playerScore;
-    pcScore.innerText = computerScore;
-};
-
-// Calculate who won
-playerClickRPS = (playerChoice) => {
-    let computerChoice = getComputerChoice();
-    let player1Score = getResult(playerChoice, computerChoice);
-    let player2Score = getResult(playerChoice, computerChoice);
-    showResult(playerScore, playerChoice, computerChoice);
-
-    console.log(totalScore['playerScore'] += playerScore);
-    console.log(totalScore['computerScore'] += computerScore);
-
-    console.log({ totalScore });
+    // if (totalScore.playerScore = 0) {
+    //     alertText.innerText = "DRAW";
+    // } else if (playerScore = 1) {
+    //     alertText.innerText = "You WIN";
+    // } else {
+    //     alertText.innerText = "You LOOSE";
+    // }
+    // myScore.innerText = totalScore.playerScore;
+    // pcScore.innerText = totalScore.computerScore;
 };
 
 // funtion to play game
 playGame = () => {
-    btnRPS.forEach(rpsButton => {
-        rpsButton.addEventListener('click', (e) => {
-            playerClickRPS(rpsButton.value);
-            getResult();
-            showResult();
-        });
-    });
-    endGame();
+    // btnRPS.forEach(rpsButton => {
+    //     rpsButton.addEventListener('click', (e) => {
+    //         playerClickRPS(rpsButton.value);
+    //         // getResult();
+    //         // showResult();
+    //     });
+    // });
+    // endGame();
 };
 
 // function to reset all the text to zero
 endGame = () => {
-    btnReset.addEventListener('click', () => {
-        myScore.innerText = 'ME: ' + '0';
-        pcScore.innerText = 'PC: ' + '0';
-    });
+    // btnReset.addEventListener('click', () => {
+    //     myScore.innerText = '0';
+    //     pcScore.innerText = '0';
+    // });
 };
 
 // Call the play game function
-playGame();
+// playGame();
